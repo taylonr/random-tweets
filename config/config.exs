9820a317fbaf1ea6db789534d3cb413bada0bf10
Nate Taylor <taylonr@gmail.com>
2016-07-02 07:16:36 -0500

    Adding heartbeat for heroku

--- config/config.exs
+++ config/config.exs
@@ -14,7 +14,8 @@ use Mix.Config
 config :quantum, cron: [
     # Every minute
     #"30 13 1,15 * *": {RandomTweets.CLI, :process}
-    "30 20 2,*/12 * *": {RandomTweets.CLI, :process}
+    "30 20 2,*/12 * *": {RandomTweets.CLI, :process},
+    "27 * * * *": {RandomTweets.Heartbeat, :alive}
 ]
 #
 # And access this configuration in your application as:
