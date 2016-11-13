b5df2f165f6eaff0eca4bbe9bb283c49900473dd
Nate Taylor <taylonr@gmail.com>
2016-11-12 19:38:37 -0600

    Adding elixir course.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule RandomTweets.Mixfile do
   def project do
     [app: :random_tweets,
      version: "0.0.1",
-     elixir: "~> 1.2",
+     elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      escript: escript,
